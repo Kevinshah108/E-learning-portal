d92e7443002ad3dacb4c300d37fc00854b17afc6
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 11:58:55 +0530

    updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,8 +3,8 @@ import jwt from "jsonwebtoken"
 export const generateToken = (studentId, res) =>{
     const token = jwt.sign({studentId}, process.env.JWT_SECRET, { expiresIn: "7d"})
 
-    res.cookie("jwt", token, { maxAge: 7*24*60*60*1000, sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    secure: process.env.NODE_ENV === "production"});
+    res.cookie("jwt", token, { maxAge: 7*24*60*60*1000, httpOnly: true, sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    "secure": process.env.NODE_ENV === "production"});
 
     return token;
 }
