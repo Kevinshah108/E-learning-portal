5d4c2d4f6d0d5fae05b14d0a6f1a5c1d7fd42a6a
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 12:24:43 +0530

    updated

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,8 +4,7 @@ export const generateToken = (studentId, res) =>{
     const token = jwt.sign({studentId}, process.env.JWT_SECRET, { expiresIn: "7d"})
 
     res.cookie("jwt", token, {
-         maxAge: 7*24*60*60*1000, 
-         httpOnly: true, 
+         maxAge: 7*24*60*60*1000,
          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
          secure: process.env.NODE_ENV === "production"
 });
