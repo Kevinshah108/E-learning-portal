9444743480bda4bee259414e5881f4847e69df9b
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 11:51:04 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
     origin: "https://e-learning-portal-frontend.vercel.app",
-    methods: ["POST", "GET", "PUT"],
+    methods: ["GET", "POST", "PUT"],
     credentials: true,
 }))
 
