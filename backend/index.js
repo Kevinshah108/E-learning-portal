e7c19f3593b73decb6e01988888d3c751a7970b1
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 11:56:09 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://e-learning-portal-frontend.vercel.app/login",
+    origin: "https://e-learning-portal-frontend.vercel.app",
     methods: ["GET", "POST", "PUT"],
     credentials: true,
 }))
