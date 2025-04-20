4184029acd5aa76a81af70b33d4af7f93144e226
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 11:27:49 +0530

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: (`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api`),
+    baseURL: 'https://e-learning-portal-backend.vercel.app/api',
     withCredentials: true,
 })
