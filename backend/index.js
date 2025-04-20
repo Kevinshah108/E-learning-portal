56499549c14eb7cf03269c2947ef876ef252e471
Kevin shah <kevinofficial108@gmail.com>
2025-04-20 09:19:41 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import courseRoutes from "./routes/course.route.js"
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+import courseRoutes from "./src/routes/course.route.js"
 
 dotenv.config();
 const app= express();
@@ -15,7 +15,8 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5174",
+    origin: "http://deploy-mern-lwhq.vercel.app",
+    methods: ["POST", "GET"],
     credentials: true
 }))
 
